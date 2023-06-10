6a0240948532ca68994bd31592e78540cd617a8e
ISAWASHUN <syun20000616@icloud.com>
2023-06-10 13:05:14 +0900

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -73,3 +73,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "bootstrap-sass",  "3.4.1"
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,24 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'StaticPages', type: :request do
-  describe '#home' do
-    it '正常にレスポンスを返すこと' do
-      get static_pages_home_path
-      expect(response).to have_http_status :ok
-    end
+RSpec.describe "StaticPages", type: :system do
+  before do
+    driven_by(:rack_test)
   end
 
-  describe '#help' do
-    it '正常にレスポンスを返すこと' do
-      get static_pages_help_path
-      expect(response).to have_http_status :ok
-    end
-  end
+  describe 'root' do
+    it 'root_pathへのリンクが2つ、help, about, contactへのリンクが表示されていること' do
+      visit root_path
+      link_to_root = page.find_all("a[href=\"#{root_path}\"]")
 
-  describe '#about' do
-    it '正常にレスポンスを返すこと' do
-      get static_pages_about_path
-      expect(response).to have_http_status :ok
+      expect(link_to_root.size).to eq 2
+      expect(page).to have_link 'Help', href: help_path
+      expect(page).to have_link 'About', href: about_path
+      expect(page).to have_link 'Contact', href: contact_path
     end
   end
 end
