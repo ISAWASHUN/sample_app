aff90ec5907c07f4e0b40ba04275b01b55d574f1
ISAWASHUN <syun20000616@icloud.com>
2023-06-12 15:40:40 +0900

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,63 +1,44 @@
-require 'rails_helper'
-
-RSpec.describe User, type: :model do
-  let(:user) { User.new(name: 'Example User',
-                        email: 'user@example.com',
-                        password: 'foobar',
-                        password_confirmation: 'foobar') }
-
-  it 'userが有効であること' do
-    expect(user).to be_valid
+RSpec.describe "Users", type: :system do
+  before do
+    driven_by(:rack_test)
   end
-
-  it 'nameが必須であること' do
-    user.name = ''
-    expect(user).to_not be_valid
-  end
-
-  it 'emailが必須であること' do
-    user.email = ''
-    expect(user).to_not be_valid
-  end
-
-  it 'nameは50文字以内であること' do
-    user.name = 'a' * 51
-    expect(user).to_not be_valid
-  end
-
-  it 'emailは255文字以内であること' do
-    user.email = "#{'a' * 244}@example.com"
-    expect(user).to_not be_valid
-  end
-
-  it 'emailが有効な形式であること' do
-    valid_addresses = %w[user@exmple.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
-    valid_addresses.each do |valid_address|
-      user.email = valid_address
-      expect(user).to be_valid
+  
+  describe '#create' do
+    context '無効な値の場合' do
+      it 'エラーメッセージ用の表示領域が描画されていること' do
+        visit signup_path
+        fill_in 'Name', with: ''
+        fill_in 'Email', with: 'user@invlid'
+        fill_in 'Password', with: 'foo'
+        fill_in 'Confirmation', with: 'bar'
+        click_button 'Create my account'
+  
+        expect(page).to have_selector 'div#error_explanation'
+        expect(page).to have_selector 'div.field_with_errors'
+      end
     end
   end
 
-  it 'emailは重複して登録できないこと' do
-    duplicate_user = user.dup
-    user.save
-    expect(duplicate_user).to_not be_valid
+  context '有効な値の場合' do
+    let(:user_params) { { user: { name: 'Example User',
+                                  email: 'user@example.com',
+                                  password: 'password',
+                                  password_confirmation: 'password' } } }
+ 
+    it '登録されること' do
+      expect {
+        post users_path, params: user_params
+      }.to change(User, :count).by 1
+    end
+ 
+    it 'users/showにリダイレクトされること' do
+      post users_path, params: user_params
+      user = User.last
+      expect(response).to redirect_to user
+    end
+    it 'flashが表示されること' do
+      post users_path, params: user_params
+      expect(flash).to be_any
+    end
   end
-
-  it 'emailは小文字でDB登録されていること' do
-    mixed_case_email = 'Foo@ExAMPle.CoM'
-    user.email = mixed_case_email
-    user.save
-    expect(user.reload.email).to eq mixed_case_email.downcase
-  end
-
-  it 'passwordが必須であること' do
-    user.password = user.password_confirmation = ' ' * 6
-    expect(user).to_not be_valid
-  end
-
-  it 'passwordは6文字以上であること' do
-    user.password = user.password_confirmation = 'a' * 5
-    expect(user).to_not be_valid
-  end
-end
+ end
